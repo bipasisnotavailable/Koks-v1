a71ed4f91310847bb60f68637601f3d1c4dbbc0f
bipasisnotavailable <schooaasch7000012@gmail.com>
2022-03-09 19:09:23 +0100

    besser
    Signed-off-by: bipasisnotavailable <schooaasch7000012@gmail.com>

--- com/thealtening/api/utils/exceptions/TheAlteningException.java
+++
@@ -1,8 +0,0 @@
-package com.thealtening.api.utils.exceptions;
-
-public class TheAlteningException extends RuntimeException
-{
-    public TheAlteningException(final String error, final String errorMessage) {
-        super(String.format("[%s]: %s", error, errorMessage));
-    }
-}
