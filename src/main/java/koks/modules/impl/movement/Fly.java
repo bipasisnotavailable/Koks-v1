53cefa033ae537e674da254ede0e038f0abac021
CHEESYBOI21 <davidluong101101@gmail.com>
2022-03-19 23:45:52 -0400

    sextico

--- src/main/java/koks/modules/impl/movement/Fly.java
+++ src/main/java/koks/modules/impl/movement/Fly.java
@@ -287,5 +287,4 @@ public class Fly extends Module {
         }
     }
 
-
 }
--- src/main/java/koks/modules/impl/utilities/Disabler.java
+++ src/main/java/koks/modules/impl/utilities/Disabler.java
@@ -3,6 +3,7 @@ package koks.modules.impl.utilities;
 import koks.event.Event;
 import koks.event.impl.EventUpdate;
 import koks.modules.Module;
+import koks.utilities.PacketUtil;
 import koks.utilities.TimeUtil;
 import koks.utilities.value.values.ModeValue;
 import net.minecraft.network.Packet;
@@ -24,15 +25,62 @@ import net.minecraft.network.play.client.C0FPacketConfirmTransaction;
 import net.minecraft.network.play.server.S08PacketPlayerPosLook;
 import koks.utilities.TimerUtil;
 import koks.event.impl.*;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Random;
+import java.util.SplittableRandom;
+import java.util.concurrent.ConcurrentLinkedDeque;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.ThreadLocalRandom;
+import net.minecraft.client.Minecraft;
+import net.minecraft.network.Packet;
+import net.minecraft.network.play.INetHandlerPlayServer;
+import net.minecraft.network.play.client.C00PacketKeepAlive;
+import net.minecraft.network.play.client.C02PacketUseEntity;
+import net.minecraft.network.play.client.C03PacketPlayer;
+import net.minecraft.network.play.client.C07PacketPlayerDigging;
+import net.minecraft.network.play.client.C08PacketPlayerBlockPlacement;
+import net.minecraft.network.play.client.C0APacketAnimation;
+import net.minecraft.network.play.client.C0BPacketEntityAction;
+import net.minecraft.network.play.client.C0CPacketInput;
+import net.minecraft.network.play.client.C0FPacketConfirmTransaction;
+import net.minecraft.network.play.server.S08PacketPlayerPosLook;
+import org.apache.commons.lang3.RandomUtils;
+
+
 
 
 public class Disabler extends Module{
+
+    int jumpTimeout = 0;
+    double yStart = 0;
+    private boolean expectedTeleport;
     public TimerUtil timer = new TimerUtil();
     public static List<Packet> transactions;
     public static List<Packet> keepAlives;
     public double x,y,z;
+    private LinkedList<Packet> packetQueue = new LinkedList();
+    private int current;
+    private static CopyOnWriteArrayList<C0FPacketConfirmTransaction> bypassList = new CopyOnWriteArrayList();
+    private static SplittableRandom random = new SplittableRandom();
+    private int memeTick;
+    private final Queue j = new ConcurrentLinkedDeque();
+    private C03PacketPlayer.C06PacketPlayerPosLook aac5QueuedPacket = null;
+    private int aac5SameReach = 5;
+    List<Packet<INetHandlerPlayServer>> packetBuffer = new ArrayList<Packet<INetHandlerPlayServer>>();
+    public int confirmtranscounter;
+    public int confirmtransactioncounter;
+    public ArrayList<Packet> delayedtransactions2 = new ArrayList();
+    public static transient CopyOnWriteArrayList<Packet> packets = new CopyOnWriteArrayList();
+    int currentTransaction = 0;
+    double x2;
+    double y2;
+    S08PacketPlayerPosLook packet;
 
-    public ModeValue<String> mode = new ModeValue<>("Mode", "Verus combat", new String[]{"Verus combat"}, this);
+
+    public ModeValue<String> mode = new ModeValue<>("Mode", "Verus Combat", new String[]{"Verus combat"}, this);
 
     public Disabler() {
         super("Disabler", "Disable anticheats", Module.Category.PLAYER);
@@ -60,14 +108,22 @@ public class Disabler extends Module{
 
     }
 
+
+
+
     @Override
     public void onEnable() {
-
-
+        switch (mode.getSelectedMode()) {
+        }
     }
+
 
     @Override
     public void onDisable() {
 
+
     }
+
+
+
 }
